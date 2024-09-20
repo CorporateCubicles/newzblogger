327584b107c86b2b8612ff3c437e0fb925d7e8a0
corporatecubicles <corporatecubicles.in@gmail.com>
2024-09-20 15:45:12 +0530

    -Post Comment, show comment

--- controller/blogController.js
+++ controller/blogController.js
@@ -28,13 +28,9 @@ const loadPost = async(req, res) =>{
 const addComment = async(req, res)=>{
     try{
 
-        console.log(req.body.post_id);
-        console.log(req.body.username);
-        console.log(req.body.comment);
-
        const post_id=  req.body.post_id;
        const  username= req.body.username;
-        const comment =  req.body.comment;
+       const comment =  req.body.comment;
        
         await Post.findByIdAndUpdate({_id:post_id }, { $push:{ "comments": {username:username,  comment: comment}}});
 
